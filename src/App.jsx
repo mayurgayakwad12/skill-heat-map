f5d7a39d62f10438705f65f42f45a84d95824f56
Mayur Gayakwad <mayurnamu11@gmail.com>
2025-02-03 15:19:34 +0530

    Update width user section

--- src/App.jsx
+++ src/App.jsx
@@ -39,65 +39,65 @@ const App = () => {
 		fetchUsers()
 	}, [])
 	return (
-		<div style={{ display: 'flex', padding: 20 }}>
-			{loading && <Loader />}
-			<div
-				style={{
-					marginTop: '5px',
-					marginRight: 40,
-					border: '1px solid #000000',
-					// padding: '10px',
-					width: 200
-				}}
-			>
-				<h3
-					style={{
-						margin: '10px auto',
-						paddingBottom: '8px',
-						display: 'flex',
-						alignItems: 'center',
-						borderBottom: '1px solid #000000',
-						cursor: 'pointer',
-						paddingLeft: '10px'
-					}}
-				>
-					Most Recommended
-				</h3>
-				{users.map((user) => {
-					const isUserSelected = userFetched.current.includes(user.id)
-					return (
-						<div
-							key={user.id}
-							style={{
-								margin: '10px auto',
-								paddingBottom: '8px',
-								width: '90%',
-								display: 'flex',
-								alignItems: 'center',
-								borderBottom: '1px solid #000000',
-								cursor: 'pointer'
-							}}
-							onClick={() => {
-								if (!isUserSelected) {
-									fetchSkills(user.id)
-								}
-							}}
-						>
-							<span
-								style={{
-									cursor: 'pointer',
-									color: isUserSelected ? 'gray' : ''
-								}}
-							>
-								👤 {user.name}
-							</span>
-						</div>
-					)
-				})}
-			</div>
-			<Table skills={skills} />
-		</div>
-	)
+    <div style={{ display: 'flex', padding: 20 }}>
+      {loading && <Loader />}
+      <div
+        style={{
+          marginTop: '5px',
+          marginRight: 40,
+          border: '1px solid #000000',
+          // padding: '10px',
+          width: '230px',
+        }}
+      >
+        <h3
+          style={{
+            margin: '10px auto',
+            paddingBottom: '8px',
+            display: 'flex',
+            alignItems: 'center',
+            borderBottom: '1px solid #000000',
+            cursor: 'pointer',
+            paddingLeft: '10px',
+          }}
+        >
+          Most Recommended
+        </h3>
+        {users.map((user) => {
+          const isUserSelected = userFetched.current.includes(user.id);
+          return (
+            <div
+              key={user.id}
+              style={{
+                margin: '10px auto',
+                paddingBottom: '8px',
+                width: '90%',
+                display: 'flex',
+                alignItems: 'center',
+                borderBottom: '1px solid #000000',
+                cursor: 'pointer',
+              }}
+              onClick={() => {
+                if (!isUserSelected) {
+                  fetchSkills(user.id);
+                }
+              }}
+            >
+              <span
+                style={{
+                  cursor: 'pointer',
+                  color: isUserSelected ? 'gray' : '',
+                }}
+              >
+                👤 {user.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+      <Table skills={skills} />
+    </div>
+  );
 }
 
 export default App
